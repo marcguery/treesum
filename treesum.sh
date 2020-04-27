4a510637406180cafd2d0f26622cc727d75ab5cf
marcantoine <marcantoine.guery@gmail.com>
2020-04-27 20:39:47 +0200

    Added parameter for file size coloring

--- treesum.sh
+++ treesum.sh
@@ -5,16 +5,17 @@ GREEN='\033[01;32m'
 BLUE='\033[01;34m'
 YELLOW='\033[01;33m'
 NC='\033[0m'
-HIG=100000
-MED=50000
-LOW=10000
 
-usage() { echo "Usage: $0 [-d <int>] [-a]" 1>&2; exit 1; }
+hig=100000
+usage() { echo "Usage: $0 [-d <int>] [-m <int>] [-a]" 1>&2; exit 1; }
 
-while getopts ":d:ah" o; do
+while getopts ":d:m:ah" o; do
     case "${o}" in
         d)
             depth=${OPTARG}
+            ;;
+        m)
+            hig=${OPTARG}
             ;;
         a)
             all=1
@@ -28,9 +29,14 @@ shift $((OPTIND-1))
 
 cmdduh='du -h'
 cmddu='du'
-if [ -n "${depth}" ]; then
+if [ -n "${depth}" -a "$depth" -eq "$depth" ]; then
     cmdduh=$cmdduh' -d '"$depth"
     cmddu=$cmddu' -d '"$depth"
+fi
+
+if [ "$hig" -eq "$hig" ]; then
+    med=$(($hig/2))
+    low=$(($hig/10))
 fi
 
 if [ -n "${all}" ]; then
@@ -48,5 +54,5 @@ sed -r 's/\t/a\/'$(printf "\x1b(0\x6d\x1b(B\x1b(0\x71\x1b(B\x1b(0\x71\x1b(B")'/g
 sed -r 's/[^\/]+\/{1}/\t/g' | \
 sed -r 's/^\t{1}//g' | \
 paste - <(echo "$sizetree" | awk -v r=$RED -v y=$YELLOW -v g=$GREEN -v b=$BLUE -v n=$NC \
-	'$2 > '"$HIG"' { print r"["$1"]"n;next } $2 >= '"$MED"' { print y"["$1"]"n;next } \
-	$2 >= '"$LOW"' { print g"["$1"]"n;next } $2 < '"$LOW"' { print b$1n;next }')
+	'$2 > '"$hig"' { print r"["$1"]"n;next } $2 >= '"$med"' { print y"["$1"]"n;next } \
+	$2 >= '"$low"' { print g"["$1"]"n;next } $2 < '"$low"' { print b$1n;next }')
